f0dce7f26108e4f97c57b4bc741bcaeb05b62929
KwangMinChoi <rhkd4560@naver.com>
2019-12-11 22:10:12 +0900

    redux 미들웨어 공부

---
+++ src/api/posts.js
@@ -0,0 +1,30 @@
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+
+// { id, title, body }
+const posts = [
+    {
+        id: 1,
+        title : 'test1',
+        body: 'body1'
+    },
+    {
+        id: 2,
+        title : 'test2',
+        body: 'body2'
+    },
+    {
+        id: 3,
+        title : 'test3',
+        body: 'body3'
+    }
+];
+
+export const getPosts = async () => {
+    await sleep(500);
+    return posts;
+}
+
+export const getPostById = async () => {
+    await(500);
+    return posts.find(post => post.id === id);
+}
