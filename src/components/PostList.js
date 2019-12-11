4d86bca5b4ed2f37243aa14314584914810b9f37
KwangMinChoi <rhkd4560@naver.com>
2019-12-11 23:59:38 +0900

    component, container 생성 후 확인

---
+++ src/components/PostList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PostList({ posts }) {
+    return(
+        <ul>
+            {
+                posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
+    );
+}
+
+export default PostList;
