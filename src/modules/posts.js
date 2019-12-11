cfa8ec27ce03be707d7a879491bf8448595c5f1e
KwangMinChoi <rhkd4560@naver.com>
2019-12-11 22:49:03 +0900

    가짜 api 생성 후 비동기 처리

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from '..//api/posts';
+import { reducerUtils } from '../lib/asyncUtils';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -14,7 +15,7 @@ export const getPosts = () => async dispatch => {
     dispatch({ type : GET_POSTS });
     // API를 호출
     try{
-        const posts = await.postsAPI.getPosts();
+        const posts = await postsAPI.getPosts();
         // 성공했을 때
         dispatch({
             type: GET_POSTS_SUCCESS,
@@ -34,7 +35,7 @@ export const getPost = id => async dispatch => {
     dispatch({ type : GET_POST });
     // API를 호출
     try{
-        const post = await.postsAPI.getPost(id);
+        const post = await postsAPI.getPost(id);
         // 성공했을 때
         dispatch({
             type: GET_POST_SUCCESS,
@@ -48,3 +49,70 @@ export const getPost = id => async dispatch => {
         });
     }
 };
+
+const initialState = {
+    posts: reducerUtils.initial(),
+    post: reducerUtils.initial()
+}
+
+//reducer
+export default function posts(state = initialState, action){
+    switch (action.type){
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: {
+                    loading: true,
+                    data: null,
+                    error: null,
+                }
+            };
+        case GET_POSTS_SUCCESS:
+            return{
+                ...state,
+                posts: {
+                    loading: false,
+                    data: action.posts,
+                    error: null
+                }
+            }
+        case GET_POSTS_ERROR:
+            return {
+                ...state,
+                posts: {
+                    loading: false,
+                    data : null,
+                    error : action.error
+                }
+            }
+        case GET_POST:
+            return {
+                ...state,
+                post: {
+                    loading: true,
+                    data: null,
+                    error: null,
+                }
+            };
+        case GET_POST_SUCCESS:
+            return{
+                ...state,
+                post: {
+                    loading: false,
+                    data: action.posts,
+                    error: null
+                }
+            }
+        case GET_POST_ERROR:
+            return {
+                ...state,
+                post: {
+                    loading: false,
+                    data : null,
+                    error : action.error
+                }
+            }
+        default:
+            return state;
+    }
+}
