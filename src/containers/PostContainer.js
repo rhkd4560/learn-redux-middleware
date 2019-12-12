0cac7429eddabee4df45d987e348c881e7d7ae09
KwangMinChoi <rhkd4560@naver.com>
2019-12-12 20:30:07 +0900

    Util함수 구현으로 코드 재활용

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -5,13 +5,15 @@ import Post from '../components/Post'
 import { reducerUtils } from '../lib/asyncUtils';
 
 function PostContainer({ postId }){
-    const { data, loading, error} = useSelector(state => state.posts.post[postId] || reducerUtils.initial());
+    const { data, loading, error} = useSelector(
+        state => state.posts.post[postId] || reducerUtils.initial()
+    );
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if(data) return;
+        if (data) return;
         dispatch(getPost(postId));
-    }, [postId, dispatch]);
+    }, [postId, dispatch, data]);
 
     if (loading && !data) return <div>로딩중...</div>
     if (error) return <div>에러 발생!</div>
--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -14,9 +14,36 @@ export const createPromiseThunk = (type, promiseCreator) => {
                 type: ERROR,
                 payload: e,
                 error: true
-            })
+            });
         }
-    }
+    };
+};
+
+
+const defaultIdSelector = param => param;
+
+export const createPromiseThunkById = (type, promiseCreator, idSelector = defaultIdSelector) => {
+    const [SUCCESS, ERROR] = [ `${type}_SUCCESS`, `${type}_ERROR` ];
+    
+    return param => async dispatch => {
+        const id = idSelector(param)
+        dispatch({ type, meta: id });
+        try{
+            const payload = await promiseCreator(param);
+            dispatch({
+                type: SUCCESS,
+                payload,
+                meta: id
+            })
+        } catch (e) {
+            dispatch({
+                type: ERROR,
+                payload: e,
+                error: true,
+                meta: id
+            });
+        }
+    };
 };
 
 //3가지 액션에 대한 reducer를 만들어서 반환
@@ -39,6 +66,41 @@ export const handleAsyncActions = (type, key, keepData) => {  //createPromiseThu
                 return{
                     ...state,
                     [key]: reducerUtils.error(action.payload)
+                };
+            default:
+                return state;
+        }
+    }
+}
+export const handleAsyncActionsById = (type, key, keepData) => { 
+    const [SUCCESS, ERROR] = [ `${type}_SUCCESS`, `${type}_ERROR` ];
+    //reducer
+    return (state, action) => {
+        const id = action.meta;
+        switch (action.type) {
+            case type:
+                return{
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.loading(keepData ? (state[key][id] && state[key][id].data) : null)
+                    }
+                };
+            case SUCCESS:
+                return{
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.success(action.payload)
+                    }
+                };
+            case ERROR:
+                return{
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.error(action.payload)
+                    }
                 };
             default:
                 return state;
