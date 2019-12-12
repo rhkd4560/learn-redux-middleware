fe61731980afc5e72f86f0bce25d552718821b63
KwangMinChoi <rhkd4560@naver.com>
2019-12-13 03:13:38 +0900

    saga를 이용한 Counter구현

--- src/index.js
+++ src/index.js
@@ -5,19 +5,29 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import ReduxThunk from 'redux-thunk';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import createSagaMiddleware from 'redux-saga';
 
 const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(ReduxThunk.withExtraArgument({history: customHistory}), logger))
+    composeWithDevTools(
+        applyMiddleware(
+            sagaMiddleware,
+            ReduxThunk.withExtraArgument({history: customHistory}),
+            logger
+        )
+    )
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Router history={customHistory}>
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,21 +1,43 @@
+import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects' //effects 명령
+
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
+const INCREASE_ASYNC = 'INCREASE_ASYNC';
+const DECREASE_ASYNC = 'DECREASE_ASYNC';
 
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-//thunk 함수
-export const increaseAsync = () => dispatch => {
-    setTimeout(() => {
-        dispatch(increase())
-    }, 1000);
-};
+//saga
+function* increaseSaga() {
+    yield delay(1000);
+    yield put(increase());  //put = dispatch와 유사
+}
 
-export const decreaseAsync = () => dispatch => {
-    setTimeout(() => {
-        dispatch(decrease())
-    }, 1000);
-};
+function* decreaseSaga() {
+    yield delay(1000);
+    yield put(decrease());
+}
+
+export function* counterSaga() {
+    yield takeEvery(INCREASE_ASYNC, increaseSaga);
+    yield takeLatest(DECREASE_ASYNC, decreaseSaga);     //가장 나중에온명령만 받음
+}
+
+// thunk 함수
+// export const increaseAsync = () => dispatch => {
+//     setTimeout(() => {
+//         dispatch(increase())
+//     }, 1000);
+// };
+
+// export const decreaseAsync = () => dispatch => {
+//     setTimeout(() => {
+//         dispatch(decrease())
+//     }, 1000);
+// };
 
 const initialState = 0;
 
